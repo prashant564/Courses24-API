18ca96c887154b2b6e04ee1b25e961583a3d14ce
Dprash <prashantdixit600@gmail.com>
2020-05-10 05:26:20 +0530

    Login and registration done

---
+++ controllers/auth.js
@@ -0,0 +1,56 @@
+const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+// @desc     Register user
+// @route    POST /api/v1/auth/register
+// @access   Public
+exports.register = asyncHandler(async (req, res, next) => {
+  const { name, email, password, role } = req.body;
+
+  // Create user
+  const user = User.create({
+    name,
+    email,
+    password,
+    role,
+  });
+
+  // Create token
+  const token = jwt.sign({ id: (await user)._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+  res.status(200).json({ success: true, token });
+});
+
+// @desc     Login user
+// @route    GET /api/v1/auth/login
+// @access   Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Validate email and password
+  if (!email || !password) {
+    return next(new ErrorResponse('Please provide an email and password', 400));
+  }
+
+  // Check for user
+  const user = await User.findOne({ email }).select('+password');
+  if (!user) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  // Check if password matches
+  const isMatch = await user.matchPassword(password);
+  if (!isMatch) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  // Create token
+  const token = jwt.sign({ id: (await user)._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+
+  res.status(200).json({ success: true, token });
+});
--- server.js
+++ server.js
@@ -14,6 +14,7 @@ connectDB();
 // Route files
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
+const auth = require('./routes/auth');
 
 const app = express();
 
@@ -28,6 +29,7 @@ if (process.env.NODE_ENV === 'development') {
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
+app.use('/api/v1/auth', auth);
 
 app.use(errorHandler);
 
