d79185544584ddad1f27f831a28cacf267b1fc86
Dprash <prashantdixit600@gmail.com>
2020-05-09 19:56:44 +0530

    Advanced results middleware done

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -11,13 +11,19 @@ const {
   getBootcampsInRadius,
 } = require('../controllers/bootcamps');
 
+const Bootcamp = require('../models/Bootcamp');
+const advancedResults = require('../middleware/advancedResults');
+
 // Include other resource routers
 const courseRouter = require('./courses');
 
 // Re-route into other resource routers
 router.use('/:bootcampId/courses', courseRouter);
 
-router.route('/').get(getBootcamps).post(createBootcamp);
+router
+  .route('/')
+  .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
+  .post(createBootcamp);
 
 router
   .route('/:id')
